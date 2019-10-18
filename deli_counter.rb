63f9296fc3f9382eb6426ee8c5a5d9fe1806bb5a
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 03:39:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def take_a_number(katz_deli, name)
      puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
   end
   
-  def now_serving(katz_deli, name)
+  def now_serving(katz_deli)
     
     if katz_deli.length < 1
       puts "There is nobody waiting to be served!"
