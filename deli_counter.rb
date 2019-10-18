f729743f9272552e4dccb155d7de5291f4441b48
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 01:49:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def line(katz_deli)
+  
+  if katz_deli.length < 1
+   puts "The line is currently empty."
+  
+  else
+  
+  output = "The line is currently:"
+  katz_deli.each.with_index {|elem,ind| output << " #{ind+1}. #{elem}"} 
+  puts output
+  end
+
+end
+
+def take_a_number(katz_deli, name)
+
+  if katz_deli.length < 1
+     katz_deli.push(name) 
+     puts "Welcome, #{name}. You are number 1 in line."
+     
+  else
+    
+     katz_deli.length > 0
+     katz_deli.each.with_index do |elem,ind| 
+     puts "Welcome, #{elem}. You are number #{index+1} in line." 
+  end
+
+ end  
+  
+
+  
