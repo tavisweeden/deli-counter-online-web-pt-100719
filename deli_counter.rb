35b74de68a41659aae941779b5511450d473f70c
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 03:45:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,7 +31,7 @@ def take_a_number(katz_deli, name)
       puts "There is nobody waiting to be served!"
     
     else
-      puts "Currently serving #{katz_deli.}."
+      puts "Currently serving #{katz_deli.first}."
       katz_deli.shift
 
   end  
