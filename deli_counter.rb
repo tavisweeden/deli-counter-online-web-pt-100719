241cdf83f52c2141166729546385af8c8af15fe4
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 02:43:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,8 +22,8 @@ def take_a_number(katz_deli, name)
     
      katz_deli.length > 0
      katz_deli.push(name)
-     katz_deli.each.with_index do |elem,ind| 
-     puts "Welcome, #{elem}. You are number #{ind+1} in line." 
+     #katz_deli.each.with_index do |elem,ind| 
+     puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
   end
 
  end  
