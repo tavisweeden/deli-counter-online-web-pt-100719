6005d1ff738f613257c7386954d44d3db6f0b942
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 02:51:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,10 @@ def take_a_number(katz_deli, name)
      puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
   end
   
-  
+  def now_serving(katz_deli, name)
+    
+    if katz_deli.length > 0
+      puts 
 
  end  
   
